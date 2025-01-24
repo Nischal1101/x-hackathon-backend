d4809f925856f02d54a5d8871369421ac0c05eb9
nischal1101 <kaflenk123@gmail.com>
2025-01-24 22:41:18 +0545

    add config file

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { JwtService } from '@nestjs/jwt';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JWT_SECRET } from 'src/config';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true,
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '2h' },
+    }),
+  ],
   providers: [AuthService, JwtService],
   controllers: [AuthController],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { PORT } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(PORT ?? 3000, () =>
+    console.log(`App is listening on port ${PORT}`),
+  );
 }
 bootstrap();
