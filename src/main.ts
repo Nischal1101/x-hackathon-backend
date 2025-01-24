7858467084da3af188b8ec102d20d2bb07192538
nischal1101 <kaflenk123@gmail.com>
2025-01-25 00:54:22 +0545

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { PORT } from './config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(PORT ?? 3000, () =>
     console.log(`App is listening on port ${PORT}`),
